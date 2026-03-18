ff93dbbbd74a64ad8f711bd78a043325a93ec47d
VoTaDuy <154273585+VoTaDuy@users.noreply.github.com>
2026-03-18 22:53:18 +0700

    first clean commit

---
+++ TDTVSystem/src/main/java/com/example/TDTVSystem/TdtvSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TDTVSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdtvSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdtvSystemApplication.class, args);
+	}
+
+}
